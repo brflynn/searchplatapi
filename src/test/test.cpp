bef7f5f0c7d325fe6b7da36eebdbc357ce8b0f26
Brendan Flynn <49290318+brflynn@users.noreply.github.com>
2025-06-04 13:10:40 -0700

    Add in helper class for queries, update tests, add in total files in index API

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -1,6 +1,5 @@
 #include "pch.h"
 #include "SearchPlatAPI.h"
-#include "CppUnitTest.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -12,21 +11,21 @@ namespace SearchPlatAPITests
 
 		TEST_METHOD(TestGetSearchManager)
 		{
-			auto searchManager = GetSearchManager();
+			auto searchManager = searchapi::GetSearchManager();
 			Assert::IsNotNull(searchManager.get());
 
 		}
 
 		TEST_METHOD(TestGetSystemIndexSearchCatalogManager)
 		{
-			auto searchCatalogManager = GetSystemIndexCatalogManager();
+			auto searchCatalogManager = searchapi::GetSystemIndexCatalogManager();
 			Assert::IsNotNull(searchCatalogManager.get());
 
 		}
 
 		TEST_METHOD(TestGetSystemIndexSearchCrawlScopeManager)
 		{
-			auto searchCrawlScopeManager = GetSystemIndexCrawlScopeManager();
+			auto searchCrawlScopeManager = searchapi::GetSystemIndexCrawlScopeManager();
 			Assert::IsNotNull(searchCrawlScopeManager.get());
 
 		}
@@ -34,37 +33,40 @@ namespace SearchPlatAPITests
 		TEST_METHOD(ValidateBuildPrimingSQL)
 		{
 			std::wstring expectedSql = L"";
-			std::vector<std::wstring> includedScopes = { GetKnownFolderScope(FOLDERID_Documents), GetKnownFolderScope(FOLDERID_Desktop) };
-			std::wstring builtSql = BuildPrimingSqlFromScopes(includedScopes, std::vector<std::wstring>());
+			std::vector<std::wstring> includedScopes = { searchapi::GetKnownFolderScope(FOLDERID_Documents), searchapi::GetKnownFolderScope(FOLDERID_Desktop) };
+			std::wstring builtSql = searchapi::internal::BuildPrimingSqlFromScopes(includedScopes, std::vector<std::wstring>());
 
-			auto documentsScope = GetKnownFolderScope(FOLDERID_Documents);
+			auto documentsScope = searchapi::GetKnownFolderScope(FOLDERID_Documents);
 			std::replace(documentsScope.begin(), documentsScope.end(), L'\\', L'/');
-			auto desktopScope = GetKnownFolderScope(FOLDERID_Desktop);
+			auto desktopScope = searchapi::GetKnownFolderScope(FOLDERID_Desktop);
 			std::replace(desktopScope.begin(), desktopScope.end(), L'\\', L'/');
 			expectedSql += L"SELECT System.ItemUrl FROM SystemIndex WHERE ( SCOPE='file:" + documentsScope + L"' OR SCOPE='file:" + desktopScope + L"')";
 
 			Assert::AreEqual(builtSql.c_str(), expectedSql.c_str());
 		}
 
-		TEST_METHOD(TestPrimeQueryAndReuse)
+		TEST_METHOD(TestFileSearchProviderPrepareForSearch)
 		{
-			std::vector<std::wstring> includedScopes = { GetKnownFolderScope(FOLDERID_Documents), GetKnownFolderScope(FOLDERID_Desktop) };
-			auto primingRowset = CreateQueryPrimingRowset(includedScopes, std::vector<std::wstring>());
-			auto reuseWhere = GetReuseWhereIDFromRowset(primingRowset);
-			Assert::AreNotSame(reuseWhere, 0ul);
+			std::vector<std::wstring> includedScopes = { searchapi::GetKnownFolderScope(FOLDERID_Documents), searchapi::GetKnownFolderScope(FOLDERID_Desktop) };
+			searchapi::FileSearchProvider searchProvider;
+			searchProvider.PrepareForSearch(includedScopes, std::vector<std::wstring>());
 		}
 
-		TEST_METHOD(TestExecuteSearchAllQueryUsePrimingQuery)
+		TEST_METHOD(TestFileSearchProviderIssueQuery)
 		{
-			std::vector<std::wstring> includedScopes = { GetKnownFolderScope(FOLDERID_Documents), GetKnownFolderScope(FOLDERID_Desktop) };
-			auto primingRowset = CreateQueryPrimingRowset(includedScopes, std::vector<std::wstring>());
-			auto reuseWhere = GetReuseWhereIDFromRowset(primingRowset);
-			auto rowset = ExecuteQueryUsingPrimingQuery(L"Find all text");
+			std::vector<std::wstring> includedScopes = { searchapi::GetKnownFolderScope(FOLDERID_Documents), searchapi::GetKnownFolderScope(FOLDERID_Desktop) };
+			auto searchProvider = searchapi::FileSearchProvider();
+			searchProvider.PrepareForSearch(includedScopes, std::vector<std::wstring>());
+			auto results = searchProvider.Search(L"Foo");
 
-			Assert::IsNotNull(rowset.get());
+			Assert::AreEqual(static_cast<unsigned long>(results.size()), 0ul);
+		}
 
-			// Enumerate the rows
-			// Ensures we got a real handle
+		TEST_METHOD(TestFileCountInIndex)
+		{
+			auto searchProvider = searchapi::FileSearchProvider();
+
+			Assert::IsTrue(searchProvider.GetTotalFilesInIndex() > 0);
 		}
 	};
 }
--- src/test/test_cache.cpp
+++ src/test/test_cache.cpp
@@ -1,7 +1,6 @@
 #include "pch.h"
 #define SEARCHPLAT_API_CACHE 1
 #include "SearchPlatAPI.h"
-#include "CppUnitTest.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -13,36 +12,55 @@ namespace SearchPlatAPITests
 
 		TEST_METHOD(TestGetSearchManager)
 		{
-			auto searchManager = GetSearchManager();
+			auto searchManager = searchapi::GetSearchManager();
 			Assert::IsNotNull(searchManager.get());
-			Assert::IsNotNull(s_cachedManager.get());
+
 		}
 
 		TEST_METHOD(TestGetSystemIndexSearchCatalogManager)
 		{
-			auto searchCatalogManager = GetSystemIndexCatalogManager();
+			auto searchCatalogManager = searchapi::GetSystemIndexCatalogManager();
 			Assert::IsNotNull(searchCatalogManager.get());
-			Assert::IsNotNull(s_cachedSystemIndexCatalogManager.get());
+
 		}
 
 		TEST_METHOD(TestGetSystemIndexSearchCrawlScopeManager)
 		{
-			auto searchCrawlScopeManager = GetSystemIndexCrawlScopeManager();
+			auto searchCrawlScopeManager = searchapi::GetSystemIndexCrawlScopeManager();
 			Assert::IsNotNull(searchCrawlScopeManager.get());
-			Assert::IsNotNull(s_cachedSystemIndexCrawlScopeManager.get());
+
 		}
 
 		TEST_METHOD(ValidateBuildPrimingSQL)
 		{
 			std::wstring expectedSql = L"";
-			std::vector<std::wstring> includedScopes = { GetKnownFolderScope(FOLDERID_Documents), GetKnownFolderScope(FOLDERID_Desktop) };
-			std::wstring builtSql = BuildPrimingSqlFromScopes(includedScopes, std::vector<std::wstring>());
+			std::vector<std::wstring> includedScopes = { searchapi::GetKnownFolderScope(FOLDERID_Documents), searchapi::GetKnownFolderScope(FOLDERID_Desktop) };
+			std::wstring builtSql = searchapi::internal::BuildPrimingSqlFromScopes(includedScopes, std::vector<std::wstring>());
+
+			auto documentsScope = searchapi::GetKnownFolderScope(FOLDERID_Documents);
+			std::replace(documentsScope.begin(), documentsScope.end(), L'\\', L'/');
+			auto desktopScope = searchapi::GetKnownFolderScope(FOLDERID_Desktop);
+			std::replace(desktopScope.begin(), desktopScope.end(), L'\\', L'/');
+			expectedSql += L"SELECT System.ItemUrl FROM SystemIndex WHERE ( SCOPE='file:" + documentsScope + L"' OR SCOPE='file:" + desktopScope + L"')";
+
+			Assert::AreEqual(builtSql.c_str(), expectedSql.c_str());
 		}
 
-		TEST_METHOD(TestPrimeQueryAndReuse)
+		TEST_METHOD(TestFileSearchProviderPrepareForSearch)
 		{
-			std::vector<std::wstring> includedScopes = { GetKnownFolderScope(FOLDERID_Documents), GetKnownFolderScope(FOLDERID_Desktop) };
-			auto primingRowset = CreateQueryPrimingRowset(includedScopes, std::vector<std::wstring>());
+			std::vector<std::wstring> includedScopes = { searchapi::GetKnownFolderScope(FOLDERID_Documents), searchapi::GetKnownFolderScope(FOLDERID_Desktop) };
+			searchapi::FileSearchProvider searchProvider;
+			searchProvider.PrepareForSearch(includedScopes, std::vector<std::wstring>());
+		}
+
+		TEST_METHOD(TestFileSearchProviderIssueQuery)
+		{
+			std::vector<std::wstring> includedScopes = { searchapi::GetKnownFolderScope(FOLDERID_Documents), searchapi::GetKnownFolderScope(FOLDERID_Desktop) };
+			searchapi::FileSearchProvider searchProvider;
+			searchProvider.PrepareForSearch(includedScopes, std::vector<std::wstring>());
+			auto results = searchProvider.Search(L"Foo");
+
+			Assert::AreEqual(static_cast<unsigned long>(results.size()), 0ul);
 		}
 	};
 }
